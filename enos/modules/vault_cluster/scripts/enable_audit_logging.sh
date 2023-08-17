200af1aaaa0661986d530f6e2e6479f9069e1417
Marc Boudreau <marc.boudreau@hashicorp.com>
2023-08-17 14:05:50 -0400

    enabling all 3 Audit Device types in enos

--- enos/modules/vault_cluster/scripts/enable_audit_logging.sh
+++ enos/modules/vault_cluster/scripts/enable_audit_logging.sh
@@ -5,4 +5,12 @@
 
 set -eux
 
+LOG_DIR="$(dirname "$LOG_FILE_PATH")"
+
+# Run ncat to listen to port 9090
+ncat -l 9090 > "$LOG_DIR/socket.log" &
+NCAT_PID=$!
+
 $VAULT_BIN_PATH audit enable file file_path="$LOG_FILE_PATH"
+$VAULT_BIN_PATH audit enable syslog tag="vault" facility="AUTH"
+$VAULT_BIN_PATH audit enable socket address="127.0.0.1:9090"
